b41911b33a4219350e9dc914eb178d724ef01c01
huannguyen310 <huanchappie0809@gmail.com>
2024-01-06 14:16:01 +0700

    NVH782_Cau2

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using NguyenVanHuan782.Models;
+
+namespace NguyenVanHuan782.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {}
+        public DbSet<Student> Student { get; set;}
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,9 @@
+namespace NguyenVanHuan782.Models
+{
+    public class Student
+    {
+        public string StudentID { get; set; }
+        public int SoDinhDanh { get; set; }
+        public float SDT { get; set; }
+    }
+}
